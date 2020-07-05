eff5030c05874781b3d7a2ec7f1ae2d5825f2a0d
rrtyagii <rtyagi1@hawk.iit.edu>
2020-07-05 00:12:39 -0500

    Added Quick Sort

--- GUI.py
+++ GUI.py
@@ -4,16 +4,17 @@ from tkinter import ttk
 import random
 from sorting_function import sorting_function
 
-
-sortingClass= sorting_function()
+sortingClass = sorting_function()
 root = Tk()
 root.title('Sorting Algorithm Visualizer')
 root.maxsize(900, 600)
 currAlgorithms = StringVar()
-arr=[]
+arr = []
+
 
 def algorithm_selected():
-    print("Algorithm Selected: "+currAlgorithms.get())
+    print("Algorithm Selected: " + currAlgorithms.get())
+
 
 def random_array_generator():
     global arr
@@ -21,50 +22,59 @@ def random_array_generator():
     arr = []
     try:
         for i in range(sizeEntryString.get()):
-            arr.insert(i, random.randint(minEntryString.get() , maxEntryString.get()))
+            arr.insert(i, random.randint(minEntryString.get(), maxEntryString.get()))
     except:
         for i in range(5):
-            arr.insert(i, random.randint(0,9))
+            arr.insert(i, random.randint(0, 9))
 
-    draw_array(arr,['#6c9ff0' for x in range(len(arr))])
+    draw_array(arr, ['#6c9ff0' for x in range(len(arr))])
 
 
-
-def draw_array(array,colourArray):
+def draw_array(array, colourArray):
     canvas.delete('all')
     canvas_height = 380
     canvas_width = 600
-    barGraph_width = canvas_width/ (len(array) + 1)
+    barGraph_width = canvas_width / (len(array) + 1)
     offset = 30
     spacing = 7
-    norm_arr = [i/max(array) for i in array]
+    norm_arr = [i / max(array) for i in array]
     for i, height in enumerate(norm_arr):
-        x1 = i* barGraph_width + offset + spacing
+        x1 = i * barGraph_width + offset + spacing
         y1 = canvas_height - height * 340
-        x2 =  (i+1) * barGraph_width + offset
+        x2 = (i + 1) * barGraph_width + offset
         y2 = canvas_height
 
-        canvas.create_rectangle(x1, y1, x2, y2, fill = colourArray[i])
-#        canvas.create_text(x1 +2, y1, anchor= SW, text= str(array[i]))
+        canvas.create_rectangle(x1, y1, x2, y2, fill=colourArray[i])
+    #        canvas.create_text(x1 +2, y1, anchor= SW, text= str(array[i]))
     root.update_idletasks()
 
 
 def sorting():
-# 'Bubble Sort', 'Insertion Sort', 'Selection Sort','Quick Sort', 'Merge Sort'
+    # 'Bubble Sort', 'Insertion Sort', 'Selection Sort','Quick Sort', 'Merge Sort'
     global arr
     if currAlgorithms.get() == 'Bubble Sort':
-        sortingClass.bubble_sort(arr, draw_array,speedScale.get())
+        sortingClass.bubble_sort(arr, draw_array, speedScale.get())
         print("starting bubble sort")
+
     if currAlgorithms.get() == 'Insertion Sort':
-        sortingClass.insertion_sort(arr,draw_array, speedScale.get())
+        sortingClass.insertion_sort(arr, draw_array, speedScale.get())
+        print("starting insertion sort")
+
     if currAlgorithms.get() == 'Selection Sort':
-        sortingClass.selection_sort(arr,draw_array, speedScale.get())
+        sortingClass.selection_sort(arr, draw_array, speedScale.get())
+        print("starting selection sort")
+
     if currAlgorithms.get() == 'Merge Sort':
-        sortingClass.merge_sort(arr,draw_array, speedScale.get())
+        sortingClass.merge_sort(arr, draw_array, speedScale.get())
+        print("starting merge sort")
+
+    if currAlgorithms.get() == 'Quick Sort':
+        sortingClass.quick_sort(arr, 0, len(arr) - 1, draw_array, speedScale.get())
+        print("starting quick sort")
 
 
 def swap_two_pos(pos_0, pos_1):
-#    x_00
+    #    x_00
     pass
 
 
@@ -74,49 +84,51 @@ def random_sort():
 
 def from_rgb(rgb):
     """Trying to translate an rgb tuple of into a tkinter friendly colour code"""
-    return "#%02x%02x%02x" %rgb
+    return "#%02x%02x%02x" % rgb
 
 
-root.config(bg = 'black')
-mainFrame = Frame(root, width = 600, height = 150, bg ='#34495e')
-mainFrame.grid(row =0, column =0, padx =10, pady=5)
+root.config(bg='black')
+mainFrame = Frame(root, width=600, height=150, bg='#34495e')
+mainFrame.grid(row=0, column=0, padx=10, pady=5)
 
-canvas = Canvas(root, width = 700, height = 420,bg= 'white')
-canvas.grid(row = 1, column = 0, padx =10, pady=5)
+canvas = Canvas(root, width=700, height=420, bg='white')
+canvas.grid(row=1, column=0, padx=10, pady=5)
 
-#Row[0]
-label=Label(mainFrame, text="Algorithm : ", bg='#b5bdc4').grid(row=0, column =0, padx =5, pady=5, sticky = W)
-algMenu= ttk.Combobox(mainFrame, textvariable=currAlgorithms, values=['Bubble Sort', 'Insertion Sort', 'Selection Sort','Quick Sort', 'Merge Sort'])
-#algMenu.grid(row=0, column =0, padx =80, pady=5)
-algMenu.grid(row=0, column =1, padx =5, pady=5, sticky = W)
+# Row[0]
+label = Label(mainFrame, text="Algorithm : ", bg='#b5bdc4').grid(row=0, column=0, padx=5, pady=5, sticky=W)
+algMenu = ttk.Combobox(mainFrame, textvariable=currAlgorithms,
+                       values=['Bubble Sort', 'Insertion Sort', 'Selection Sort', 'Quick Sort', 'Merge Sort'])
+# algMenu.grid(row=0, column =0, padx =80, pady=5)
+algMenu.grid(row=0, column=1, padx=5, pady=5, sticky=W)
 algMenu.current(3)
 
-speedScale = Scale(mainFrame, from_=0.001, to=0.5, length =200, digits=2, resolution=0.001, orient= HORIZONTAL, label = 'Select Speed[s]')
+speedScale = Scale(mainFrame, from_=0.001, to=0.5, length=200, digits=2, resolution=0.001, orient=HORIZONTAL,
+                   label='Select Speed[s]')
 speedScale.grid(row=0, column=5, padx=5, pady=5, sticky=E)
-#speedScale.pack()
+# speedScale.pack()
 
 
+# Button(mainFrame, text='Select', command = selected_algo, bg='light blue').grid(row=1, column =0, padx =5, pady=5, sticky = W )
+Button(mainFrame, text='Generate Data', command=random_array_generator, bg='#b5bdc4').grid(row=0, column=2, padx=5,
+                                                                                           pady=5)
+Button(mainFrame, text="Start the Sort", command=sorting, bg='#b5bdc4').grid(row=0, column=3, padx=5, pady=5)
 
-#Button(mainFrame, text='Select', command = selected_algo, bg='light blue').grid(row=1, column =0, padx =5, pady=5, sticky = W )
-Button(mainFrame, text='Generate Data', command = random_array_generator, bg='#b5bdc4').grid(row=0, column =2, padx =5, pady=5)
-Button(mainFrame, text = "Start the Sort", command=sorting, bg='#b5bdc4').grid(row=0, column =3, padx =5, pady=5)
-
-#Row[1]
-Label(mainFrame, text = 'Size of the array: ', bg ='#b5bdc4').grid(row =1, column=0, padx=5, pady=5, sticky= W)
-sizeEntryString=tkinter.IntVar()
-sizeEntry = Entry(mainFrame,textvariable=sizeEntryString).grid(row =1, column=1, padx=5, pady=5, sticky=W)
-#sizeEntryString = sizeEntry.get()
+# Row[1]
+Label(mainFrame, text='Size of the array: ', bg='#b5bdc4').grid(row=1, column=0, padx=5, pady=5, sticky=W)
+sizeEntryString = tkinter.IntVar()
+sizeEntry = Entry(mainFrame, textvariable=sizeEntryString).grid(row=1, column=1, padx=5, pady=5, sticky=W)
+# sizeEntryString = sizeEntry.get()
 
 
-Label(mainFrame, text = 'Min Value: ', bg ='#b5bdc4').grid(row =1, column=2, padx=5, pady=5, sticky= W)
+Label(mainFrame, text='Min Value: ', bg='#b5bdc4').grid(row=1, column=2, padx=5, pady=5, sticky=W)
 minEntryString = tkinter.IntVar()
-minEntry = Entry(mainFrame, textvariable=minEntryString).grid(row =1, column=3, padx=5, pady=5, sticky=W)
-#minEntryString = minEntry.get()
+minEntry = Entry(mainFrame, textvariable=minEntryString).grid(row=1, column=3, padx=5, pady=5, sticky=W)
+# minEntryString = minEntry.get()
 
 
-Label(mainFrame, text = 'Max Value: ', bg ='#b5bdc4').grid(row =1, column=4, padx=5, pady=5, sticky= W)
+Label(mainFrame, text='Max Value: ', bg='#b5bdc4').grid(row=1, column=4, padx=5, pady=5, sticky=W)
 maxEntryString = tkinter.IntVar()
-maxEntry = Entry(mainFrame, textvariable=maxEntryString).grid(row =1, column=5, padx=5, pady=5, sticky=W)
-#maxEntryString = maxEntry.get()
+maxEntry = Entry(mainFrame, textvariable=maxEntryString).grid(row=1, column=5, padx=5, pady=5, sticky=W)
+# maxEntryString = maxEntry.get()
 
 root.mainloop()
--- sorting_function.py
+++ sorting_function.py
@@ -1,159 +1,188 @@
-#import random
+# import random
 import time
-#from GUI import draw_array
-#from datetime import timedelta
+
+
+# from GUI import draw_array
+# from datetime import timedelta
 
 
 class sorting_function:
-    def bubble_sort(self,lst,draw_array, timetick):
-        for _ in range(len(lst)-1):
-            for j in range(len(lst)-1):
-                if lst[j] > lst[j+1]:
-                    lst[j],lst[j+1] = lst[j+1], lst[j]
-                    draw_array(lst, ['#8351a1' if x == j or x==j+1 else '#6c9ff0' for x in range(len(lst))])
+    def bubble_sort(self, lst, draw_array, timetick):
+        for _ in range(len(lst) - 1):
+            for j in range(len(lst) - 1):
+                if lst[j] > lst[j + 1]:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                    draw_array(lst, ['#8351a1' if x == j or x == j + 1 else '#6c9ff0' for x in range(len(lst))])
                     time.sleep(timetick)
 
     def insertion_sort(self, lst, draw_arra, timetick):
         for i in range(1, len(lst)):
             key = lst[i]
-            j = i-1
-            while j>=0 and lst[j] > key:
-                lst[j+1] = lst[j]
-                j-=1
-            lst[j+1] = key
-            draw_arra(lst, ['#8351a1' if x == j or x==j+1 else '#6c9ff0' for x in range(len(lst))] )
+            j = i - 1
+            while j >= 0 and lst[j] > key:
+                lst[j + 1] = lst[j]
+                j -= 1
+            lst[j + 1] = key
+            draw_arra(lst, ['#8351a1' if x == j or x == j + 1 else '#6c9ff0' for x in range(len(lst))])
             time.sleep(timetick)
 
     def selection_sort(self, lst, draw_array, timetick):
         for i in range(len(lst)):
             min_index = i
-            for j in range(i+1, len(lst)):
+            for j in range(i + 1, len(lst)):
                 if lst[j] < lst[min_index]:
                     min_index = j
             if min_index != i:
                 lst[i], lst[min_index] = lst[min_index], lst[i]
-                draw_array(lst,['#8351a1' if x == j or x==j+1 else '#6c9ff0' for x in range(len(lst))] )
+                draw_array(lst, ['#8351a1' if x == j or x == j + 1 else '#6c9ff0' for x in range(len(lst))])
                 time.sleep(timetick)
 
-#     def merge_sort(self, lst, drawData, timeTick):
-# #        print(lst)
-#         if len(lst) <=1:
-#             return lst
-#         else:
-#             mid = len(lst)//2
-#             firstHalf= self.merge_sort(lst[mid:])
-#             secondHalf = self.merge_sort(lst[:mid])
-#             result = self._merge(firstHalf, secondHalf, drawData, timeTick)
-# #            print(result)
-#             return result
-#
-#     def _merge(self, lst1, lst2, drawData, timeTick):
-#
-#         merge = []
-#
-#         i=j=0
-#         while i<len(lst1) or  j<len(lst2):
-#             if j == len(lst2) or (i<len(lst1) and lst1[i] < lst2[j] ):
-#                 merge.append(lst1[i])
-#                 i+=1
-#             else:
-#                 merge.append(lst2[j])
-#                 j+=1
-#         return merge
+    #     def merge_sort(self, lst, drawData, timeTick):
+    # #        print(lst)
+    #         if len(lst) <=1:
+    #             return lst
+    #         else:
+    #             mid = len(lst)//2
+    #             firstHalf= self.merge_sort(lst[mid:])
+    #             secondHalf = self.merge_sort(lst[:mid])
+    #             result = self._merge(firstHalf, secondHalf, drawData, timeTick)
+    # #            print(result)
+    #             return result
+    #
+    #     def _merge(self, lst1, lst2, drawData, timeTick):
+    #
+    #         merge = []
+    #
+    #         i=j=0
+    #         while i<len(lst1) or  j<len(lst2):
+    #             if j == len(lst2) or (i<len(lst1) and lst1[i] < lst2[j] ):
+    #                 merge.append(lst1[i])
+    #                 i+=1
+    #             else:
+    #                 merge.append(lst2[j])
+    #                 j+=1
+    #         return merge
     def merge_sort(self, lst, drawData, timeTick):
-        self._merge_sort(lst, 0, len(lst)-1, drawData, timeTick)
+        self._merge_sort(lst, 0, len(lst) - 1, drawData, timeTick)
 
     def _merge_sort(self, lst, left, right, drawData, timeTick):
-        if left<right:
-            mid = (left+right)//2
-            self._merge_sort(lst, left, mid, drawData, timeTick )
-            self._merge_sort(lst, mid+1, right, drawData, timeTick )
+        if left < right:
+            mid = (left + right) // 2
+            self._merge_sort(lst, left, mid, drawData, timeTick)
+            self._merge_sort(lst, mid + 1, right, drawData, timeTick)
             self._merge(lst, left, mid, right, drawData, timeTick)
 
     def _merge(self, lst, left, midpoint, right, drawData, tickTime):
-        drawData(lst, self.colour_codes_mergeSort(len(lst), left, midpoint, right))
+        drawData(lst, self._colour_codes_mergeSort(len(lst), left, midpoint, right))
         time.sleep(tickTime)
 
-        L = lst[left:midpoint+1]
-        R = lst[midpoint+1:right+1]
+        L = lst[left:midpoint + 1]
+        R = lst[midpoint + 1:right + 1]
 
-        i=j=0
+        i = j = 0
 
-        for idx in range(left, right+1):
-            if i< len(L) and j< len(R):
+        for idx in range(left, right + 1):
+            if i < len(L) and j < len(R):
                 if L[i] < R[j]:
                     lst[idx] = L[i]
-                    i+=1
+                    i += 1
                 else:
                     lst[idx] = R[j]
-                    j+=1
-            elif i< len(L):
+                    j += 1
+            elif i < len(L):
                 lst[idx] = L[i]
-                i+=1
+                i += 1
             else:
                 lst[idx] = R[j]
-                j+=1
-        drawData(lst, ['#8351a1' if x >= left or x <=right else '#6c9ff0' for x in range(len(lst))] )
+                j += 1
+        drawData(lst, ['#6c9ff0' if x >= left or x <= right else '#8351a1' for x in range(len(lst))])
         time.sleep(tickTime)
 
-    def quick_sort(self, arr, start, end): #, draw_array, timetick):
-        if start <end:
-            index = self._partition(arr, start, end)#, draw_array, timetick)
-            self.quick_sort(arr, start, index-1)#, draw_array, timetick)
-            self.quick_sort(arr, index+1, end)#, draw_array, timetick)
+    def quick_sort(self, arr, start, end, draw_array, timetick):
+        if start < end:
+            index = self._partition(arr, start, end, draw_array, timetick)
+            self.quick_sort(arr, start, index - 1, draw_array, timetick)
+            self.quick_sort(arr, index + 1, end, draw_array, timetick)
 
-    def _partition(self, arr, start, end):#, draw_array, timetick):
-        i = start - 1
+    def _partition(self, arr, start, end, draw_array, timetick):
+        i = start
         pivot = arr[end]
+
+        draw_array(arr, self._colour_for_quicksort(len(arr), start, end, i, i))
+        time.sleep(timetick)
 
         for j in range(start, end):
             if arr[j] < pivot:
-                i+=1
+                draw_array(arr,
+                           self._colour_for_quicksort(length=len(arr), left=start, right=end, partition=i, currIdx=j,
+                                                      swapped=True))
+                time.sleep(timetick)
                 arr[i], arr[j] = arr[j], arr[i]
-        arr[i+1], arr[end] = arr[end], arr[i+1]
-        return (i+1)
-#         high  = end
-#         while True:
-#             while low<=high and arr[high] >= pivot:
-#                 high = high -1
-#             while low<= high and arr[low] <= pivot:
-#                 low = low + 1
-#             if low<= high:
-#                 arr[low], arr[high] =arr[high], arr[low]
-# #                draw_array(arr,['#8351a1' if x == j or x==j+1 else '#6c9ff0' for x in range(len(lst))] )
-# #                time.sleep(timetick)
-#             else:
-#                 break
-#         arr[start], arr[low] = arr[low], arr[low]
-#         return low
+                i += 1
 
-    def colour_codes_mergeSort(self, length, left, middle, right):
-        colour_codes =[]
+            draw_array(arr, self._colour_for_quicksort(length=len(arr), left=start, right=end, partition=i, currIdx=j))
+            time.sleep(timetick)
+
+        draw_array(arr, self._colour_for_quicksort(length=len(arr), left=start, right=end, partition=i, currIdx=end,
+                                                   swapped=True))
+        time.sleep(timetick)
+
+        arr[i], arr[end] = arr[end], arr[i]
+        return (i)
+
+    #         high  = end
+    #         while True:
+    #             while low<=high and arr[high] >= pivot:
+    #                 high = high -1
+    #             while low<= high and arr[low] <= pivot:
+    #                 low = low + 1
+    #             if low<= high:
+    #                 arr[low], arr[high] =arr[high], arr[low]
+    # #                draw_array(arr,['#8351a1' if x == j or x==j+1 else '#6c9ff0' for x in range(len(lst))] )
+    # #                time.sleep(timetick)
+    #             else:
+    #                 break
+    #         arr[start], arr[low] = arr[low], arr[low]
+    #         return low
+
+    def _colour_codes_mergeSort(self, length, left, middle, right):
+        colour_codes = []
         for i in range(length):
-            if i>=left and i<=right:
-                if i>=left and i<=middle:
+            if i >= left and i <= right:
+                if i >= left and i <= middle:
                     colour_codes.append("#8351a1")
                 else:
-                    colour_codes.append("#f48cfc")
+                    colour_codes.append("#6c9ff0")
             else:
-                colour_codes.append('#6c9ff0')
+                colour_codes.append('#f48cfc')
         return colour_codes
 
-
-    def _colour_for_quicksort(self, length, left, right, partition, currIdx, swapped=false):
+    def _colour_for_quicksort(self, length, left, right, partition, currIdx, swapped=False):
         colourArray = []
         for i in range(length):
-            if i<= left and i:
-                pass
+            if i <= left and i <= right:
+                colourArray.append('#f48cfc')
+            else:
+                colourArray.append('#8351a1')
+            if i == right:
+                colourArray[i] = "#6c9ff0"
+            elif i == partition:
+                colourArray[i] = '#8aafe9'
+            elif i == currIdx:
+                colourArray[i] = '#b5c8eb'
 
+            if swapped:
+                if i == partition or i == currIdx:
+                    colourArray[i] = '#03fc77'
+        return colourArray
 
-sorti = sorting_function()
-lst = [29,99,27,41,66,28,44,78,87,19,31,76,58,88,83,97,12,21,44]
-print(lst)
-print("#Sorted List")
-#start_time = time.process_time()
-
-(sorti.quick_sort(lst, 0, len(lst)-1))
-print(lst)
-#elapsed = (time.process_time() -start_time)
-#print(mergeSort)
+# sorti = sorting_function()
+# lst = [29,99,27,41,66,28,44,78,87,19,31,76,58,88,83,97,12,21,44]
+# print(lst)
+# print("#Sorted List")
+# #start_time = time.process_time()
+#
+# (sorti.quick_sort(lst, 0, len(lst)-1))
+# print(lst)
+# #elapsed = (time.process_time() -start_time)
+# #print(mergeSort)
